7015786045117193788ea7f809cedd9768ae9c51
Lenin Josue Moreno <nb604@hotmail.com>
2023-03-17 16:22:23 -0500

    Fundamentos de  Angular ngfor,ngmodel,ngif,ngSubmit

---
+++ src/recap.ts
@@ -0,0 +1,16 @@
+// import { NgIfContext } from "@angular/common";
+
+// const username: string | number = 'lenin';
+// const sum = (a: number, b: number) => {
+//   return a + b;
+// };
+// sum(1, 3);
+
+// class Persona {
+//   constructor(public age: number,public lastName: string) {
+
+//   }
+// }
+
+// const lenin = new Persona(10,'Moreno')
+// lenin.age
